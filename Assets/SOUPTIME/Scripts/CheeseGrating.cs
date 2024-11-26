917955f99a700be7f0a27f65aa1fc61bf697f6c1
SirDante830 <dantec830@gmail.com>
2024-11-25 19:56:20 -0500

    Trying to fix cheese strings and socket

--- Assets/SOUPTIME/Scripts/CheeseGrating.cs
+++ Assets/SOUPTIME/Scripts/CheeseGrating.cs
@@ -9,13 +9,13 @@ public class CheeseGrating : MonoBehaviour
     private int cutCount = 0;
     public int maxCutCount = 3;
     private GameObject cheeseToCut;
-    [SerializeField] private string targetLayer = "Cheese";
+    [SerializeField] private string targetLayer = "Uncut Cheese";
     private void OnTriggerEnter(Collider other)
     {
         GameObject collidedObject = other.gameObject;
         cheeseToCut = collidedObject;
         Transform collidedTransform = collidedObject.transform;
-        if (collidedObject.layer == LayerMask.NameToLayer("Cheese"))
+        if (collidedObject.layer == LayerMask.NameToLayer(targetLayer))
         {
             Debug.Log($"Collided with {collidedObject.name}, which has the layer: {targetLayer}");
             cutCount++;
